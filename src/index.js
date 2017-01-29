3ee2d4476def6d36067a4c7b17bfb2814891d353
HeskeyBaozi <hezhiyu233@foxmail.com>
2017-01-29 17:34:59 +0800

    normalize

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import router from './router';
 import store from './store';
+import 'normalize.css';
+import './index.css';
 
 const app = new Vue({
-    template: `<router-view></router-view>`,
     store,
     router
 });
